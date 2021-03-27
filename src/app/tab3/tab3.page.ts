e525aa5750c2aeca81ea22cdc8170ff7275c7fcb
Valerie Ann Cabales <valerieann.caballes@evsu.edu.ph>
2021-03-27 12:26:32 +0800

    Activity 3 Add Contact

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  contName = "";
+  contNumber = ""
+  contacts = [];
   constructor() {}
 
+  saveContact() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+    }
+    this.contacts.push(contact);
+    this.clearField();
+    console.log(this.contacts);
+  }
+  clearField(){
+    this.contName = "";
+    this.contNumber = "";
+  }
+  deleteContact(index){
+    let status = confirm("Are you sure you want to delete this contact?");
+
+    if(status == true){
+        this.contacts.splice(index, 1);
+      }
+    else{
+      console.log("Cancelled");
+    }
+  }
 }
